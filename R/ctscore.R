23c557ee597c46cd566b2ba2e3f52c190a1d4975
thibautjombart <thibautjombart@gmail.com>
2026-06-22 23:50:10 +0200

    wip on ctscore; not working yet

--- R/ctscore.R
+++ R/ctscore.R
@@ -13,14 +13,29 @@
 #'   of probabilities giving p(0 day), p(1 day), p(2 days) ... or as a 
 #'   `distcrete` object as returned by distcrete::distcrete()
 #'   
-ctscore <- function(x, incub) {
-  if (!inherits(x, "ctdata")) {
+ctscore <- function(x, incub, t = Sys.Date()) {
+  ## process inputs
+  
+   if (!inherits(x, "ctdata")) {
     msg <- "'x' should be a ctdata object as returned by make_ctdata()"
     stop(msg)
   }
-  if (nrow(x) == 0L) {
-    msg <- "'x' has 0 row; at least one data entry is required"
-    stop(msg)
-  }
+ 
+  incub <- process_incub(incub)
+ 
+  
+  ## split the data.frame by contact ID and calculate the score for each contact
+  list_x <- split(x, x$contact_id)
+  
+  ## calculate the probability of infection
+  list_scores <- lapply(
+    list_x, 
+    function(e) calculate_ctscore(p_inf = e$p_infection, 
+                                  e = e$date, 
+                                  s = e$last_visit, 
+                                  t = t, 
+                                  incub = incub)
+    )
+   
   
 }
--- tests/testthat/test_ctscore.R
+++ tests/testthat/test_ctscore.R
@@ -3,15 +3,40 @@ test_that(
   {
     msg <- "'x' should be a ctdata object as returned by make_ctdata\\(\\)"
     expect_error(ctscore("pmspodf"), msg)
-    
-    x <- make_ctdata(
-      contact_id = integer(0), 
-      date = integer(0), 
-      type = character(0), 
-      location = character(0)
-    )
-    msg <- "x' has 0 row; at least one data entry is required"
-    expect_error(ctscore(x), msg)
       
+    msg <- "'incub' should be a numeric vector or a distcrete object"
+    expect_error(
+      ctscore(make_ctdata(contact_id = 1, date = Sys.Date()), incub = "pmspodf"), 
+      msg
+    )
+    
+  }
+)
+
+
+
+
+test_that(
+  "ctscore gives expected results", 
+  {
+    ## We first make a ctdata object
+    ## 
+    ## 'null' exposure is used as a sanity check to ensure that the function
+    ## correctly handles zero probabilities of infection
+    ## We also make an incubation time distribution with zero mass at day 1,
+    ## where the score should be zero.
+    x <- make_ctdata(
+      contact_id = c(1, 1, 2, 3, 4), 
+      date = Sys.Date() - c(6, 4, 5, 1, 5),
+      type = c("normal", "funeral", "normal", "normal", "null"),
+      location = "some-town",
+      infection_proba = list(normal = 0.2, funeral = 0.9, null = 0),
+      last_visit = Sys.Date() - c(4, 4, 1, 1, 3)
+    )
+    ## Incub gives the pmf of the incubation time from time 0 to length(incub) -
+    ## 1
+   # incub <- c(0, 0, 1, 2, 4, 3, 2, 1)
+    
+   # res <- ctscore(x, incub)
   }
 )
