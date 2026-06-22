fa6b6c3dff49c340b66b2fe4d1a48cfd7c9f213c
thibautjombart <thibautjombart@gmail.com>
2026-06-22 23:31:03 +0200

    tests for internal calculate_ctscore

---
+++ tests/testthat/test_calculate_ctscore.R
@@ -0,0 +1,82 @@
+test_that(
+  "calculate_ctscore gives correct results", 
+  {
+    ## We generate toy data to calculate scores
+    w <- c(0, 0, 1, 2, 3, 2, 0.5)
+    incub <- process_incub(w)
+    
+    x <- data.frame(
+      date_exposure = c(0, 2, 3),
+      p_exposure = c(0.1, 0.9, 0.1)
+    )
+    
+    ## expect 0 because too early post first exposure
+    res <- calculate_ctscore(
+      p_inf = x$p_exposure, 
+      e = x$date_exposure,
+      s = -1, 
+      t = 1, 
+      incub = incub
+    )
+    expect_equal(res, 0)
+    
+    ## expect score=p_inf because very late after exposures
+    res <- calculate_ctscore(
+      p_inf = x$p_exposure, 
+      e = x$date_exposure,
+      s = -1, 
+      t = 100, 
+      incub = incub
+    )
+    expect_equal(res, sum(calculate_p_infection(x$p_exposure)))
+    
+    ## with a single exposure and no followup
+    res <- calculate_ctscore(
+      p_inf = 0.2, 
+      e = 0,
+      s = -1, 
+      t = 5, 
+      incub = incub
+    )
+    expect_equal(res, 0.2* sum(incub(0:5)))
+    
+    ## with a single exposure and some followup
+    res <- calculate_ctscore(
+      p_inf = 0.2, 
+      e = 0,
+      s = 4, 
+      t = 5, 
+      incub = incub
+    )
+    expect_equal(res, 0.2* sum(incub(5)) / sum(incub(5:10)))
+    
+    ## several exposures, no followup
+    res <- calculate_ctscore(
+      p_inf = x$p_exposure, 
+      e = x$date_exposure,
+      s = -1, 
+      t = 4, 
+      incub = incub
+    )
+    p_inf <- c(0.1, 0.9*0.9, 0.1*0.9*0.1)
+    p_symp <- c(sum(incub(0:4)), sum(incub(0:2)), sum(incub(0:1)))
+    expect_equal(res, sum(p_inf * p_symp))
+    
+    ## several exposures, some followup
+    res <- calculate_ctscore(
+      p_inf = x$p_exposure, 
+      e = x$date_exposure,
+      s = 3, 
+      t = 5, 
+      incub = incub
+    )
+    p_inf <- c(0.1, 0.9*0.9, 0.1*0.9*0.1)
+    p_symp <- c(
+      sum(incub(4:5))/sum(incub(4:10)), 
+      sum(incub(2:3))/sum(incub(2:10)), 
+      sum(incub(2))/sum(incub(1:10))
+      )
+    expect_equal(res, sum(p_inf * p_symp))
+    
+  }
+)
