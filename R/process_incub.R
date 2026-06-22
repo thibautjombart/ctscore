0d690dbb127122fe4634cb4d99c1c1c23da96ab8
thibautjombart <thibautjombart@gmail.com>
2026-06-22 12:08:45 +0200

    process_incub now returns a function

--- R/process_incub.R
+++ R/process_incub.R
@@ -8,9 +8,9 @@
 #' 
 #' @param x the incubation time distribution, either as numeric or distcrete 
 #'   object
-#' @param max_delay the maximum duration of the incubation time; defaults to 100
-#'   days
 #' 
+#' @returns A function outputing the PMF of the incubation time distribution,
+#'   with support from 0 to max_delay days.
 
 process_incub <- function(x, max_delay = 100) {
   if (!inherits(x, c("numeric", "distcrete"))) {
@@ -20,18 +20,33 @@ process_incub <- function(x, max_delay = 100) {
   
   ## if x is a distcrete object, convert to a vector of probabilities
   if (inherits(x, "distcrete")) {
-    x <- x$d(0:max_delay)
+    out <- x$d
   }
   
-  ## check that the incubation period distribution is non-negative
-  if (any(x < 0)) {
+  ## if x is a numeric object, we turn it into a function returning the PMF
+  if (inherits(x, "numeric")) {
+    ## check that the incubation period distribution is non-negative
+    if (any(x < 0)) {
     msg <- "'x' should be non-negative"
     stop(msg)
+    }
+   
+    ### closure function to attach x to the function outputing the PMF of the
+    ### incubation time distribution;
+    ###
+    
+    f_out <- function(x) {
+      x <- x / sum(x)
+      n <- length(x) - 1L
+      # t is a vector of delays
+      function(t) {
+        are_in_range <- t >= 0 & t <= n
+        res <- double(length(t))
+        res[are_in_range] <- x[t[are_in_range] + 1L]
+        res
+      }
+    }
+    out <- f_out(x)
   }
-  
-  ## keep only first max_delay values, rescale to sum to 1
-  x <- head(x, max_delay + 1L)
-  x <- x / sum(x)
-  
-  x
+  out
 }
--- tests/testthat/test_process_incub.R
+++ tests/testthat/test_process_incub.R
@@ -18,19 +18,20 @@ test_that(
   {
     ## basic test for rescaling
     x <- c(1, 2, 3)
-    expect_equal(process_incub(x), x/sum(x))
+    res <- process_incub(x)
+    expect_true(inherits(res, "function"))
+    expect_equal(res(0:2), x/sum(x))
     
-    ## test going over max_length
-    x <- rep(1, 20)
-    res <- process_incub(x, max_delay = 12)
-    expect_equal(length(res), 13L)
-    expect_equal(res, rep(1/13, 13))  
-  
+    ## test that out-of-range values are set to 0
+    expect_equal(res(-1), 0)
+    expect_equal(res(3), 0)
+    
     ## check distcrete objects work
-    x <- distcrete::distcrete("norm", mean = 12, sd = 10, interval = 1L)
-    res <- process_incub(x, max_delay = 21)
-    expect_equal(sum(res), 1L)
-    expect_true(all(res >= 0))
+    x <- distcrete::distcrete("exp", rate = 2, interval = 1L)
+    res <- process_incub(x)
+    expect_true(inherits(res, "function"))
+    expect_equal(sum(res(0:100)), 1L)
+    expect_true(all(res(0) >= 0))
   }
 )
     
